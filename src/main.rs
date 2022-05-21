9abe3b246ee9aee0db7875557fbea4d5c4b93951
jo! <me@j0.lol>
2022-05-21 18:54:21 +0100

    fmt and clean

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{fs, time};
-use std::thread::sleep;
 use feed_rs::model::Entry;
 use feed_rs::parser;
+use std::thread::sleep;
+use std::{fs, time};
 
 use serde_derive::{Deserialize, Serialize};
-use toml::Value;
 use toml::value::Array;
+use toml::Value;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 struct Config {
@@ -15,14 +15,14 @@ struct Config {
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 struct Cache {
-    cache: Array,
+    cache: Vec<String>,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 struct Feed {
-    url: Value,
-    pfp: Value,
-    webhook: Value,
+    url: String,
+    pfp: String,
+    webhook: String,
 }
 
 struct WebhookEmbed {
@@ -33,41 +33,39 @@ struct WebhookEmbed {
 
 fn main() {
     for i in load_feeds() {
-            let mut entries = parse_feed( fetch(i.url.as_str().unwrap().to_string()) );
-            if entries.len() > 5 {
-                entries.drain(5..entries.len());
-                        }
+        let mut entries = parse_feed(fetch(i.url));
+        if entries.len() > 5 {
+            entries.drain(5..entries.len());
+        }
 
-            for j in entries {
-                let mut link = String::from("Placeholder");
-                for k in j.links {
-                    if k.rel == Some("alternate".to_string()) {
-                        link = k.href.as_str().to_string()
-                    }
+        for j in entries {
+            let mut link = String::from("Placeholder");
+            for k in j.links {
+                if k.rel == Some("alternate".to_string()) {
+                    link = k.href.as_str().to_string()
                 }
-                if link == String::from("Placeholder") {
-                    unimplemented!("Weird atom feed?")
-                }
+            }
+            if link == String::from("Placeholder") {
+                unimplemented!("Weird atom feed?")
+            }
 
-                                println!("Processing {}", link);
+            println!("Processing {}", link);
 
-                if !is_cached(link.as_str()) {
+            if !is_cached(link.as_str()) {
+                dbg!(&j.authors);
 
-                                        dbg!(&j.authors);
+                add_cache(link.as_str());
+                let webhook = WebhookEmbed {
+                    content: link,
+                    username: j.authors[0].name.clone().to_string(),
+                    pfp: i.pfp.clone(),
+                };
 
-                    add_cache(link.as_str());
-                    let webhook = WebhookEmbed {
-                        content: link,
-                        username: j.authors[0].name.clone().to_string(),
-                        pfp: i.pfp.as_str().unwrap().to_string()
-                    };
-
-                    sleep(time::Duration::from_secs(1));
-                    hook(i.webhook.as_str().unwrap(), webhook)
-                }
-
+                sleep(time::Duration::from_secs(1));
+                hook(i.webhook.clone(), webhook)
             }
         }
+    }
 }
 
 fn parse_feed(url: String) -> Vec<Entry> {
@@ -80,27 +78,31 @@ fn load_feeds() -> Vec<Feed> {
         Ok(value) => value,
         Err(_) => unimplemented!(),
     };
-    let config : Config = toml::from_str(config.as_str() ).unwrap();
+    let config: Config = toml::from_str(config.as_str()).unwrap();
 
-    config.feeds.clone().to_vec()
+    config.feeds
 }
 
 fn fetch(url: String) -> String {
     let body: String = ureq::get(url.as_str())
-        .call().unwrap()
-        .into_string().unwrap();
+        .call()
+        .unwrap()
+        .into_string()
+        .unwrap();
 
     body
 }
 
-fn hook(url: &str, webhook: WebhookEmbed) {
-    ureq::post(url)
+fn hook(url: String, webhook: WebhookEmbed) {
+    ureq::post(&url)
         .send_json(ureq::json!({
-            "avatar_url": webhook.pfp,
-            "username": webhook.username,
-            "content": format!("🚨 {}", webhook.content)
-      })).unwrap()
-        .into_string().unwrap();
+              "avatar_url": webhook.pfp,
+              "username": webhook.username,
+              "content": format!("🚨 {}", webhook.content)
+        }))
+        .unwrap()
+        .into_string()
+        .unwrap();
 }
 
 fn is_cached(value: &str) -> bool {
@@ -108,9 +110,9 @@ fn is_cached(value: &str) -> bool {
         Ok(value) => value,
         Err(_) => unimplemented!(),
     };
-    let config: Config = toml::from_str(config.as_str() ).unwrap();
+    let config: Config = toml::from_str(config.as_str()).unwrap();
 
-    config.cache.cache.to_vec().contains(&toml::Value::from(value))
+    config.cache.cache.to_vec().contains(&value.to_string())
 }
 fn add_cache(value: &str) {
     let config = match fs::read_to_string("../feeds.toml") {
@@ -118,9 +120,9 @@ fn add_cache(value: &str) {
         Err(_) => unimplemented!(),
     };
 
-    let mut config: Config = toml::from_str(config.as_str() ).unwrap();
+    let mut config: Config = toml::from_str(config.as_str()).unwrap();
 
-    config.cache.cache.push(toml::Value::from(value));
+    config.cache.cache.push(value.parse().unwrap());
 
     let string = toml::to_string(&config).unwrap();
 
